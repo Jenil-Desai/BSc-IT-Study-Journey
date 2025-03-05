d796cf3fee2d191e918fe0c79d65b59d61c5d9e7
Jenil Desai <jenildev91@gmail.com>
2025-03-05 09:24:09 +0530

    v2.26.1
    ⚪️ Advance Java Unit - 4
    ⚪️ Advance Python Unit - 4
    ⚪️ Web Programming Unit - 4

---
+++ Semester-4/Java/Unit-3/Unit3/src/java/program2.java
@@ -0,0 +1,73 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/program2"})
+public class program2 extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+                out.println("<head>");
+                    out.println("<title>Servlet program2</title>");
+                out.println("</head>");
+                out.println("<body>");
+                    String uname = request.getParameter("uname");
+                    String pwd = request.getParameter("pwd");
+                    
+                    if (uname.toLowerCase() == "admin" && pwd == "123") {
+                        out.println("<p>Welcome...!</p>");
+                    } else {
+                        out.println("<p>Invalid Username And Password</p>");
+                    }
+                out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
