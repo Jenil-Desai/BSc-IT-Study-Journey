e29e0eee0d0864841ce354e67f2d4df3694943ca
Jenil Desai <jenildev91@gmail.com>
2025-03-27 23:18:49 +0530

    v4.30.1
    ⚪️ Advance Java Unit - 4
    ⚪️ Advance Python Unit - 4
    ⚪️ Web Programming Unit - 4

--- Semester-4/Web Programming - 2/Unit-3/Cookies/script.php
+++ Semester-4/Web Programming - 2/Unit-3/Cookies/script.php
@@ -1,12 +1,8 @@
 <?php
-    setcookie("name", "John Watkin", time()+3600, "/","", 0);
+setcookie("name", "John Watkin", time() + 3600, "/", "", 0);
 
-    if (isset($_COOKIE["name"]))
-    {
-        echo "Welcome " . $_COOKIE["name"];
-    }
-    else
-    {
-        echo "Sorry... Not recognized";
-    }
-?>
+if (isset($_COOKIE["name"])) {
+    echo "Welcome " . $_COOKIE["name"];
+} else {
+    echo "Sorry... Not recognized";
+}
