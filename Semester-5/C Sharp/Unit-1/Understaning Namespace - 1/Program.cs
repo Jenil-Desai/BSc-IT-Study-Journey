b200a256e2e73f078761d0c3de394c0c07c13881
Jenil Desai <jenildev91@gmail.com>
2025-07-14 15:25:58 +0530

    v5.0.1
    - C Sharp | Unit-1
     - Understanding Namespace - 1

---
+++ Semester-5/C Sharp/Unit-1/Understaning Namespace - 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Motorsports
+{
+    public class Cars
+    {
+        public static void displayBrand()
+        {
+            Console.WriteLine("Hyundai");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Motorsports.Cars.displayBrand();
+    }
+}
