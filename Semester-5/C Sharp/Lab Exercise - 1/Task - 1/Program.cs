ff68b88df2dc42aa15c00f05567edd135efb93a5
Jenil Desai <jenildev91@gmail.com>
2025-07-16 17:56:53 +0530

    v5.1.1
    - C Sharp | Lab Excerise - 1
     - Task 1

---
+++ Semester-5/C Sharp/Lab Exercise - 1/Task - 1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Hello World! ");
+Console.Write("I will print on the same line.");
+Console.WriteLine(3 + 3);
+
+string name = "John";
+Console.WriteLine(name);
+
+int myNum = 18;
+Console.WriteLine(myNum);
+
+int myNum2;
+myNum2 = 18;
+Console.WriteLine(myNum2);
+
+int myNum3 = 9;
+myNum3 = 18;
+Console.WriteLine(myNum3);
+
+int num = 5;
+double myDoubleNum = 5.99D;
+char myLetter = 'D';
+bool myBool = true;
+string myText = "Hello";
+
+Console.WriteLine(num);
+Console.WriteLine(myDoubleNum);
+Console.WriteLine(myLetter);
+Console.WriteLine(myBool);
+Console.WriteLine(myText);
